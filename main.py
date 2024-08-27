a2e15d9c9c421b71d4d8fe28a6d6955b72d2738e
David Abramson <davidonthethird@gmail.com>
2024-08-27 13:29:04 -0400

    Added OCR conf check from original scan, fixed error where github doesnt upload empty folders by putting in txt files.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import PIL
 import pandas as pd
 import pytesseract
 from PIL import Image
@@ -23,12 +24,24 @@ OCR.
 
 Note: This is only for text recognition, not text detection.
 '''
+
+# Put your directory here for the project OCRModelTrainFormating (If you don't put full directories for this part, it can come up with an
+# error occasionally)
+directory = ''
+if not directory:
+    raise ValueError('Please enter a valid directory')
+
+# Need to download tesseract and put your directory here
+pytesseract.pytesseract.tesseract_cmd = r''
+if not pytesseract.pytesseract.tesseract_cmd:
+    raise ValueError('Please enter a valid tesseract directory')
+
 # ~~~ STEP 1 ~~~
 # Extract text box
 
 # Open CSV File from scanned OCR
 # CSV File needs to
-with open('ocr_out.csv') as current_csv:
+with open(f'{directory}/ocr_out.csv') as current_csv:
     # Read into Pandas DB
     full_df = pd.read_csv(current_csv)
 
@@ -47,10 +60,10 @@ with open('ocr_out.csv') as current_csv:
             continue
         else:
             # Convert into numpy (runs magnitudes faster)
-            np_df = page_df[['min_x', 'min_y', 'width', 'height', 'text']].to_numpy()
+            np_df = page_df[['min_x', 'min_y', 'width', 'height','conf', 'text']].to_numpy()
 
             # Open image of full page
-            img = Image.open(f'Original Images/scanned_{page_num}.jpg')
+            img = Image.open(f'{directory}/Original Images/scanned_{page_num}.jpg')
 
             # Enumerate through each line in the database
             for (line_num, line) in enumerate(np_df, 1):
@@ -69,16 +82,16 @@ with open('ocr_out.csv') as current_csv:
 
                 new_img = img.crop(tuple([min_x, min_y, max_x, max_y]))
                 new_img = new_img.resize((new_len, new_wid))
-                new_img.save(rf'Test Images/page_{page_num}_word_{line_num}.jpg')
+                new_img.save(rf'{directory}/Test Images/page_{page_num}_word_{line_num}.jpg')
 
                 # Append to JSON with File Name:Word
-                with open('Jsons/training.json') as f:
+                with open(f'{directory}/Jsons/training.json') as f:
                     data = json.load(f)
 
-                new_json = {f"page_{page_num}_word_{line_num}.jpg": line[-1]}
+                new_json = {f"page_{page_num}_word_{line_num}.jpg": {"text": line[-1], "conf": line[-2]}}
                 data.update(new_json)
 
-                with open('Jsons/training.json', "w") as f:
+                with open(f'{directory}/Jsons/training.json', "w") as f:
                     json.dump(data, f)
 
 # ~~~ STEP 2 ~~~
@@ -86,39 +99,20 @@ with open('ocr_out.csv') as current_csv:
 # Scans each text box using a different OCR (I chose pytesseract)
 # Saves Confidence and Text output into a 2nd json (OCR_checker.json)
 
-# Put your directory here for the text boxes (If you dont put full directories for this part, it can come up with an
-# error occasionally)
-directory = 'C:\Python Projects\OCRModelTrainFormating\Test Images'
-
-# Need to download tesseract and put your dirrectory here
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
 # Set timer
 start_time = time.time()
 full_dict = {"filename": {"text": "text", "conf": "conf"}}
 
 # iterate through all text boxes
-for filename in os.listdir(directory):
-    f = os.path.join(directory, filename)
+for filename in os.listdir(f'{directory}/Test Images'):
+    f = os.path.join(f'{directory}/Test Images', filename)
     if os.path.isfile(f):
 
         # Open Image file
-        with Image.open(f'Test Images/{filename}') as im:
-            # Pytesseract
-            data = pytesseract.image_to_data(im, output_type=pytesseract.Output.DATAFRAME)
-            data = data.dropna()
-
-            # Get text
-            text_df = data['text'].astype('str')
-            text = ''.join(text_df.to_list())
-
-            # Get confidence
-            conf_data = data['conf']
-            conf = conf_data.mean()
-
-            # If scan doesn't work with standard configuration, it rescans with config meant for single word scans
-            if data.empty:
-                data = pytesseract.image_to_data(im, config='--psm 10', output_type=pytesseract.Output.DATAFRAME)
+        try:
+            with Image.open(f'{directory}/Test Images/{filename}') as im:
+                # Pytesseract
+                data = pytesseract.image_to_data(im, output_type=pytesseract.Output.DATAFRAME)
                 data = data.dropna()
 
                 # Get text
@@ -127,18 +121,38 @@ for filename in os.listdir(directory):
 
                 # Get confidence
                 conf_data = data['conf']
-                conf = float(conf_data.mean())
+                conf = conf_data.mean()
 
-            print(f'{filename}:{text}-{conf}')
+                # If scan doesn't work with standard configuration, it rescans with config meant for single word scans
+                if data.empty:
+                    data = pytesseract.image_to_data(im, config='--psm 10', output_type=pytesseract.Output.DATAFRAME)
+                    data = data.dropna()
 
-            # Add to dict only if text is found in image
-            if text == '\n' or not text or text is None:
+                    # Get text
+                    text_df = data['text'].astype('str')
+                    text = ''.join(text_df.to_list())
+
+                    # Get confidence
+                    conf_data = data['conf']
+                    conf = float(conf_data.mean())
+
+                print(f'{filename}:{text}-{conf}')
+
+                # Add to dict only if text is found in image
+                if text == '\n' or not text or text is None:
+                    continue
+                else:
+                    full_dict.update({filename: {'text': text, 'conf': conf}})
+
+        except PIL.UnidentifiedImageError:
+            if '.txt' in filename:
                 continue
             else:
-                full_dict.update({filename: {'text': text, 'conf': conf}})
+                raise PIL.UnidentifiedImageError(f'cannot identify image in file: {f}')
+
 
 # Save all to JSON
-with open('C:\Python Projects\OCRModelTrainFormating\Jsons\OCR_checker.json', "w") as f:
+with open(f'{directory}/Jsons/OCR_checker.json', "w") as f:
     json.dump(full_dict, f)
 
 # Get time elapsed
@@ -178,14 +192,16 @@ for filename_ocr, values_ocr in ocr_checker.items():
 
     # Get text from initial dataset
     if filename_ocr in filename_training:
-        text_training = training[filename_ocr]
+        text_training = training[filename_ocr]['text']
+        conf_training = training[filename_ocr]['conf']
+
         word = ''
 
         # If there is low confidence or a discrepancy, display text box for manual data entry
-        if text_training != text_ocr or conf_ocr < 85:
+        if text_training != text_ocr or conf_ocr < 85 or conf_training < 85:
 
             # Show image
-            imgplot = plt.imshow(mpimg.imread(f"C:/Python Projects/OCRModelTrainFormating/Test Images/{filename_ocr}"))
+            imgplot = plt.imshow(mpimg.imread(f"{directory}/Test Images/{filename_ocr}"))
             plt.ion()
             plt.title(text_training)
             plt.show()
@@ -195,26 +211,26 @@ for filename_ocr, values_ocr in ocr_checker.items():
 
             # If there is an input that is not a space, we update the word with the new word
             if not word == " " and word:
-                final_check_dict.update({filename_ocr: {'text': word, 'conf': conf_ocr}})
+                final_check_dict.update({filename_ocr: text_training})
             # If there is no input, we keep the initial word
             else:
-                final_check_dict.update({filename_ocr: {'text': text_training, 'conf': conf_ocr}})
+                final_check_dict.update({filename_ocr: text_training})
 
             plt.close()
 
         # IF there is high confidence and no discrepancy, we keep the initial word
         else:
-            final_check_dict.update({filename_ocr: {'text': text_training, 'conf': conf_ocr}})
+            final_check_dict.update({filename_ocr: text_training})
 
         # Only save if we want to keep the dataset (no space-bar entry)
         if not word == " ":
-            source = f"C:/Python Projects/OCRModelTrainFormating/Test Images/{filename_ocr}"
-            destination = f"C:/Python Projects/OCRModelTrainFormating/Final Images/{filename_ocr}"
+            source = f"{directory}/Test Images/{filename_ocr}"
+            destination = f"{directory}/Final Images/{filename_ocr}"
             shutil.copyfile(source, destination)
 
     i += 1
     print(f'Completed: {i}/{tot}')
 
 # Save Json
-with open("C:/Python Projects/OCRModelTrainFormating/Jsons/final_check.json", "w") as f:
+with open(f"{directory}/Jsons/final_check.json", "w") as f:
     json.dump(final_check_dict, f)
